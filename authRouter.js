5a293d71255e696cabc13065220b8010aab64a6d
HuziMuzi <laif17860@mail.ru>
2022-12-03 16:39:21 +0300

    added middle-w. of validate fields

--- authRouter.js
+++ authRouter.js
@@ -1,9 +1,13 @@
 const Router = require('express')
 const router = new Router()
 const controller = require('./authController.js')
+const {check} = require('express-validator')
 
 
-router.post('/registration', controller.registration)
+router.post('/registration', [
+    check('username', 'Имя пользователя не можжет быть пустым').notEmpty(),
+    check('password', 'Пароль должен быть больше 4 и короче 12 символов ').isLength({min:4, max:12})
+],controller.registration)
 router.post('/login', controller.login)
 router.get('/users', controller.getUsers)
 
